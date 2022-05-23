66a616940abb289fdfe0c81b0a41b521e70bd034
yoon <cheekyoon888@gmail.com>
2022-05-23 14:50:48 +0900

    java source

---
+++ ArrayTest1.java
@@ -0,0 +1,33 @@
+/* 학습 내용
+ * 1. 배열 학습
+ * 2. 특징
+ *  1. 다수의 데이터를 하나의 변수로 관리
+ *  2. 각각의 데이터는 고유한 index로 관리
+ *  	-index는 0부터 시작
+ *  3. 기본타입과 참조타입으로 배열 생성 및 활용가능
+ *  4. 생성된 배열의 크기는 수정 불가
+ *  	- 데이터수가 가변적인 경우 차후에 java.util에서 list API활용 예정
+ *  5. 배열이 생성시 length라는 배열 크기값 보유한 변수 자동 생성
+ *  	-java script 언어도 동일
+ *  3. 문법
+ *   1. 타입[] 변수 or 타입 변수[]
+ */
+
+package step01_syntax;
+
+public class ArrayTest1 {
+	//기본 타입으로 배열
+	
+	
+	
+	//String 객체 타입으로 배열
+	
+	
+	//Person 객체 타입으로 배열
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
